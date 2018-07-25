4c6b66b694a242a24aa60638106045ce8c53b67d
Ryan Davis <rwdavisx@gmail.com>
2018-07-24 23:23:56 -0400

    Added responsiveness to AppBar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -8,14 +8,16 @@ import RyLogo from '../../svg/RyLogo';
 
 const styles = {
   root: {
-    display: 'grid'
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   logo: {
     fontSize: 48
   },
-  links: {
-    display: 'flex',
-    justifyContent: 'space-evenly'
+  link: {
+    alignSelf: 'center',
+    'padding-left': 10,
   }
 };
 
@@ -25,15 +27,23 @@ const RyAppBar = props => {
     <AppBar classes={classes.root} position={'static'}>
       <Toolbar>
         <Grid container spacing={12}>
-          <Grid item xs={4}>
+          <Grid item md={4} sm={2} xs={12}>
             <RyLogo className={classes.logo}/>
           </Grid>
-          <Grid item xs={8} className={classes.links}>
-            <Button>HOME</Button>
-            <Button>INTERESTS</Button>
-            <Button>ABOUT</Button>
-            <Button>RESUME</Button>
-            <Button>CONTACT</Button>
+          <Grid item md={1} sm={2} xs={12} className={classes.link}>
+            <Button fullWidth>HOME</Button>
+          </Grid>
+          <Grid item md={1} sm={2} xs={12} className={classes.link}>
+            <Button fullWidth>INTERESTS</Button>
+          </Grid>
+          <Grid item md={1} sm={2} xs={12} className={classes.link}>
+            <Button fullWidth>ABOUT</Button>
+          </Grid>
+          <Grid item md={1} sm={2} xs={12} className={classes.link}>
+            <Button fullWidth>RESUME</Button>
+          </Grid>
+          <Grid item md={1} sm={2} xs={12} className={classes.link}>
+            <Button fullWidth>CONTACT</Button>
           </Grid>
         </Grid>
       </Toolbar>
