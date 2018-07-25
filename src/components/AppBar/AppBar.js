dc35d0d735e18770cc67e2aa01ee3d212805bb47
Ryan Davis <rwdavisx@gmail.com>
2018-07-24 22:13:13 -0400

    Changed grid implementation to Mui Grid Component

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -1,45 +1,43 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import SvgIcon from '@material-ui/core/SvgIcon';
 import Button from '@material-ui/core/Button';
 import RyLogo from '../../svg/RyLogo';
 
 const styles = {
   root: {
-    display: 'grid',
-    'grid-template-columns': '2fr 2fr'
+    display: 'grid'
   },
   logo: {
-    fontSize: 48,
-    display: 'flex',
-    flex: '1 1',
+    fontSize: 48
   },
   links: {
     display: 'flex',
-    justifyContent: 'space-evenly',
-    flex: 3,
+    justifyContent: 'space-evenly'
   }
 };
 
 const RyAppBar = props => {
   const { classes } = props;
   return (
-    <div>
-      <AppBar position={'static'}>
-        <Toolbar className={classes.root}>
-          <RyLogo className={classes.logo}/>
-          <div className={classes.links}>
+    <AppBar classes={classes.root} position={'static'}>
+      <Toolbar>
+        <Grid container spacing={12}>
+          <Grid item xs={4}>
+            <RyLogo className={classes.logo}/>
+          </Grid>
+          <Grid item xs={8} className={classes.links}>
             <Button>HOME</Button>
             <Button>INTERESTS</Button>
             <Button>ABOUT</Button>
             <Button>RESUME</Button>
             <Button>CONTACT</Button>
-          </div>
-        </Toolbar>
-      </AppBar>
-    </div>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
 
