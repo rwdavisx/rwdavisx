69a0f679989c060fd493f7f27d541924df8f72c5
Ryan Davis <rwdavisx@gmail.com>
2018-08-10 01:30:19 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import Theme from './utils/Theme';
 import RyAppBar from './components/AppBar/AppBar';
-import WelcomeBox from './components/WelcomeBox/WelcomeBox';
 import Intro from './components/Intro/Intro';
-import RwdLogo from './svg/RwdLogo';
 
 class App extends Component {
   render() {
